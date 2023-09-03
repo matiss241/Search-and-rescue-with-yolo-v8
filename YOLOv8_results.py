69fc84295d825dbd9808e98c7de380d66ab476cb
Maitss <macudars@gmail.com>
2023-09-04 02:42:34 +0300

    Add optimizer experiment

--- YOLOv8_results.py
+++ YOLOv8_results.py
@@ -4,6 +4,6 @@ from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO("C:/Users/MC00202/Desktop/YOLOv8/best.pt")
+model = YOLO("C:/Users/MC00202/Desktop/YOLOv8/training/testing_hp/sgd/weights/best.pt")
 
 model.predict(source="0", show=True, conf=0.5)
